6e318b49c61edfc21f55b259768fb1a27599f2db
Pandele George <pandelegeorge@yahoo.com>
2015-05-26 23:04:52 +0300

    new site

--- index.php
+++ index.php
@@ -1,340 +1,199 @@
 <?php
 $idp = $_GET['idp'];
 if (empty($idp)) {
-	$idp = 1;
+	$idp = 3;
 }
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Oana Resume</title>
-		<link rel="stylesheet" href="oanacv.css" type="text/css" />
+<html>
+<head>
+<link rel="stylesheet" href="css/header.css">
 <?php
 if($idp==1){
 ?>
-		<link href="jquery.thumbnailScroller.css" rel="stylesheet" type="text/css" />
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-		<script src="jquery-ui-1.8.13.custom.min.js"></script>
+<link rel="stylesheet" href="css/font-awesome.min.css">
 <?php
 }
 ?>
+<style>
+	body{
+		padding:0;
+		margin:0;
+		background-image: url(backgroundcvoana.jpg);
+		background-size: cover;
+		filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='backgroundcvoana.jpg',sizingMethod='scale')
+		background-color:#000000;
+		background-position: 50% 50%;
+		background-repeat: no-repeat no-repeat;
+		text-align: center;
+	}
+	.container {
+	  padding-top: 10px;
+      margin: 0 auto
+	 
+    }
+	
+	#slides {
+	  display: none
+	}
 
-<?php
-if($idp==2){
-?>		
-		<link rel="stylesheet" href="montoolsscrollgalery/demo.css" type="text/css" />
-		<script type="text/javascript" src="montoolsscrollgalery/mootools-core-1.3.1-full-nocompat.js"></script>
-		<script type="text/javascript" src="montoolsscrollgalery/mootools-1.3.1.1-more.js"></script>
-		<script type="text/javascript" src="montoolsscrollgalery/scrollGallery.js"></script>
-		<script type="text/javascript">
-			window.addEvent('domready', function() {
-				var scrollGalleryObj = new scrollGallery();
-			});
-		</script>
-		<script src="lightbox/js/jquery-1.7.2.min.js"></script>
-		<script src="lightbox/js/lightbox.js"></script>
-		<link href="lightbox/css/lightbox.css" rel="stylesheet" />
-<?php
-}
-?>
-	</head>
-	<body>
-		<div id="wrapper">
-			<div id="resume_header">
-				<div id="resume_menu">
-					<ul>
-						<li>
-							<a href="index.php?idp=1">Home</a>
-						</li>
-						<li>
-							<a href="index.php?idp=2">Work</a>
-						</li>
-						<li>
-							<a href="index.php?idp=3">Resume</a>
-						</li>
-					</ul>
-				</div>
-			</div>
+	#slides .slidesjs-navigation {
+	  margin-top:5px;
+	}
+	a.slidesjs-next,
+	a.slidesjs-previous,
+	a.slidesjs-play,
+	a.slidesjs-stop {
+	  background-image: url(img/btns-next-prev.png);
+	  background-repeat: no-repeat;
+	  display:block;
+	  width:12px;
+	  height:18px;
+	  overflow: hidden;
+	  text-indent: -9999px;
+	  float: left;
+	  margin-right:5px;
+	}
 
-			<?php
-if($idp==1){
-			?>
+	a.slidesjs-next {
+	  margin-right:10px;
+	  background-position: -12px 0;
+	}
 
-			<br />
-			<div id="resume_contact_me"> Contact me at: oana_harcau@yahoo.com </div>
-			<br /><br />
-			<div id="autocad">
-			<div id="autocadscris">
-			<b>Autocad</b>
-			</div>
+	a:hover.slidesjs-next {
+	  background-position: -12px -18px;
+	}
 
-			</div>
-			<div id="website">
-			<div id="websitescris">
-			<b>Websites</b>
-			</div>
+	a.slidesjs-previous {
+	  background-position: 0 0;
+	}
 
-			</div>
-			<div id="autocadwebsite">
+	a:hover.slidesjs-previous {
+	  background-position: 0 -18px;
+	}
 
-			</div>
-			<div id="studiomax">
-			<div id="studiomaxscris">
-			3D Studio Max
-			</div>
+	a.slidesjs-play {
+	  width:15px;
+	  background-position: -25px 0;
+	}
 
-			</div>
-			<div id="print">
-			<div id="printscris">
-			Print
-			</div>
+	a:hover.slidesjs-play {
+	  background-position: -25px -18px;
+	}
 
-			</div>
-			<div id="studiomaxprint">
+	a.slidesjs-stop {
+	  width:18px;
+	  background-position: -41px 0;
+	}
 
-			</div>
-			<div id="slideshowbackground">
-			<div id="tS2" class="jThumbnailScroller">
-			<div class="jTscrollerContainer">
-			<div class="jTscroller">
-			<a href="#"><img src="images/slideshow/slideshow1.png" /></a>
-			<a href="#"><img src="images/slideshow/slideshow2.png" /></a>
-			<a href="#"><img src="images/slideshow/slideshow3.png" /></a>
-			<a href="#"><img src="images/slideshow/slideshow4.png" /></a>
-			<a href="#"><img src="images/slideshow/slideshow5.png" /></a>
-			<a href="#"><img src="images/slideshow/slideshow6.png" /></a>
-			</div>
-			</div>
-			<a href="#" class="jTscrollerPrevButton"></a>
-			<a href="#" class="jTscrollerNextButton"></a>
-			</div>
-			</div>
+	a:hover.slidesjs-stop {
+	  background-position: -41px -18px;
+	}
 
-			<?php
-			}elseif($idp==2){
-			?>
-			<div id="worksectiontitle">
-				House Project
-			</div>
-			<div id="workholepicturebackground">
-				
-					<div id="gallery">
+	.slidesjs-pagination {
+	  margin: 7px 0 0;
+	  float: right;
+	  list-style: none;
+	}
 
-						
-						<div id="scrollGalleryFoot">
-							<div id="imagearea">
-								<div id="imageareaContent">
-									<a href="images/fotos/image1.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image1.jpg" alt="" /></a>
-									<a href="images/fotos/image2.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image2.jpg" alt="" /></a>
-									<a href="images/fotos/image3.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image3.jpg" alt="" /></a>
-									<a href="images/fotos/image4.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image4.jpg" alt="" /></a>
-									<a href="images/fotos/image5.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image5.jpg" alt="" /></a>
-									<a href="images/fotos/image6.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image6.jpg" alt="" /></a>
-									<a href="images/fotos/image7.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image7.jpg" alt="" /></a>
-									<a href="images/fotos/image7.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image8.jpg" alt="" /></a>
-									<a href="images/fotos/image9.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image9.jpg" alt="" /></a>
-									<a href="images/fotos/image10.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image10.jpg" alt="" /></a>
-									<a href="images/fotos/image11.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image11.jpg" alt="" /></a>
-									<a href="images/fotos/image12.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image12.jpg" alt="" /></a>
-									<a href="images/fotos/image13.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image13.jpg" alt="" /></a>
-									<a href="images/fotos/image14.jpg" rel="lightbox[roadtrip]"><img  src="images/fotos/image14.jpg" alt="" /></a>
+	.slidesjs-pagination li {
+	  float: left;
+	  margin: 0 1px;
+	}
 
-								</div>
-							</div>
-						</div>
-						<div id="scrollGalleryHead">
-							<div id="thumbarea">
-								<div id="thumbareaContent">
-									<img  src="images/fotos/small_image1.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image2.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image3.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image4.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image5.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image6.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image7.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image8.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image9.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image10.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image11.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image12.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image13.jpg" width="120" height="80" alt="" />
-									<img  src="images/fotos/small_image14.jpg" width="120" height="80" alt="" />
-								</div>
-							</div>
-						</div>
+	.slidesjs-pagination li a {
+	  display: block;
+	  width: 13px;
+	  height: 0;
+	  padding-top: 13px;
+	  background-image: url(img/pagination.png);
+	  background-position: 0 0;
+	  float: left;
+	  overflow: hidden;
+	}
 
-					</div>
-				
-			</div>
-			<?php
-			}elseif($idp==3){
-			?>
+	.slidesjs-pagination li a.active,
+	.slidesjs-pagination li a:hover.active {
+	  background-position: 0 -13px
+	}
 
-			<div id="resumescrisleft">
-				Name
-				<br />
-				Oana Cosmina Harcau
-				<br />
-				<br />
-				About
-				<br />
-				Hello, my name is Oana Cosmina Harcau and I’m an
-				<br />
-				engeneer after I gratueted
-				<br />
-				Polytechnic University of Bucharest .
-				<br />
-				<br />
-				Education
-				<br />
-				Highschool: National College "Matei Basarab"
-				<br />
-				University: Polytechnic University of Bucharest
-				<br />
-				<br />
-				Courses, certificates and awards
-				<br />
-				At ATC (Autodesk Authorized Training Center (ATC):
-				<br />
-				&nbsp; &nbsp; - Autocad level I-2D
-				<br />
-				&nbsp; &nbsp; - Autocad level II-3D
-				<br />
-				&nbsp; &nbsp; - Intensive course 3D Studio Max + Vray
-				<br />
-				<br />
-				Foreign languages
-				<br />
-				English (Advanced)
-				<br />
-				<br />
-				Professional experience
-				<br />
-				Engineer, Autocad Drafting, SC VEGRA SRL, Bucharest
-				<br />
-				<br />
-				Responsibilities:
-				<br />
-				&nbsp; &nbsp; - Achievement 2D and 3D building plans
-				<br />
-				&nbsp; &nbsp; - Floor plan drawings from paper sketches or scanned paper and
-				<br />
-				&nbsp; &nbsp; drawings to 2D
-				<br />
-				&nbsp; &nbsp; - Layer structure - using a structure in accordance with your
-				<br />
-				&nbsp; &nbsp; specifications
-				<br />
-				&nbsp; &nbsp; - Object representation - walls, doors, windows and other objects
-				<br />
-				&nbsp; &nbsp; - Geometric correspondence between floor plans, sections and facades
-				<br />
-				&nbsp; &nbsp; with different companies in the Netherlands and France
-				<br />
-				&nbsp; &nbsp; - Achievment template sites
-				<br />
-				&nbsp; &nbsp; - Flyers, photo modifications, banners
-				<br />
-				&nbsp; &nbsp; - Graphic magazine pages, cover book graphics
-				<br />
-				&nbsp; &nbsp; - Book achievement pages, layout page, pictures modifications in
-				<br />
-				&nbsp; &nbsp; the book pages, book design, cover book design
-				<br />
-				&nbsp; &nbsp; - Promotion of the “Archibus” product by email campaign
-				<br />
-				Layout for :
-				<br />
-				&nbsp; &nbsp; http://3dcadvegra.com
-				<br />
-				&nbsp; &nbsp; http://www.archibus.ro
-				<br />
-			</div>
-			<div id="resumescrisright">
-				<img src="images/oanapoza.png">
-				<br />
-				<br />
-				Bucharest, Romania
-				<br />
-				<br />
-				Email
-				<br />
-				<br />
-				&nbsp; &nbsp; oana_harcau@yahoo.com
-				<br />
-				<br />
-				Specialties
-				<br />
-				<br />
-				&nbsp; &nbsp; autocad, photoshop,
-				<br />
-				&nbsp; &nbsp; 3d studio max
-				<br />
-				<br />
-				Skills
-				<br />
-				<br />
-				Advanced level:
-				<br />
-				&nbsp; &nbsp; - Adobe Photoshop
-				<br />
-				&nbsp; &nbsp; - Autocad
-				<br />
+	.slidesjs-pagination li a:hover {
+	  background-position: 0 -26px
+	}
 
-				Medium Level:
-				<br />
-				&nbsp; &nbsp; - Revit Architecture 2010
-				<br />
-				&nbsp; &nbsp; - 3D Studio Max
-				<br />
-				&nbsp; &nbsp; - Illustrator
-				<br />
-				&nbsp; &nbsp; - Indesign
-				<br />
+	#slides a:link,
+	#slides a:visited {
+	  color: #333
+	}
 
-				Satisfied Level:
-				<br />
-				&nbsp; &nbsp; - Html, Css
-				<br />
-				&nbsp; &nbsp; - Microsoft Word, Microsoft
-				<br />
-				&nbsp; &nbsp; - Excel,Microsoft Outlook
-				<br />
-			</div>
-			<?php
+	#slides a:hover,
+	#slides a:active {
+	  color: #9e2020
+	}
+
+	.navbar {
+	  overflow: hidden
+	}
+
+	#slides {
+	  display: none
+	}
+
+	/* For tablets & smart phones */
+	@media screen (max-width: 767px) {
+	  body {
+		padding-left: 20px;
+		padding-right: 20px;
+	  }
+	  .container {
+		width: auto
+	  }
+	}
+
+	/* For smartphones */
+	@media screen (max-width: 480px) {
+	  .container {
+		width: auto
+	  }
+	}
+
+	/* For smaller displays like laptops */
+	@media (min-width: 768px) and (max-width: 1200px) {
+	  .container {
+		width: 724px
+	  }
+	}
+    @media (min-width: 1200px) and (max-width: 1280px) {
+	  .container {
+		width: 900px
+	  }
+	}
+	/* For larger displays */
+	@media (min-width: 1280px) {
+	  .container {
+		width: 1200px
+	  }
+	}
+</style>
+
+
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+</head>
+<body>
+
+	<?php
+		include("header.php");
+	?>
+	
+	<?php
+			if($idp==1){
+   				include("home.php");
+			}elseif($idp==3 or $idp==4 or $idp==5){
+				include("work.php");
+			}elseif($idp==6){
+			    include("resume.php");
 			}
-			?>
+	?>
+	
 
-			<div id="spaceafterslideshow">
-				Contact
-			</div>
-			<div id="contactbackground">
-
-			</div>
-		</div>
-		<script>
-			jQuery.noConflict();
-			(function($) {
-				window.onload = function() {
-					$("#tS2").thumbnailScroller({
-						scrollerType : "hoverPrecise",
-						scrollerOrientation : "horizontal",
-						scrollSpeed : 2,
-						scrollEasing : "easeOutCirc",
-						scrollEasingAmount : 600,
-						acceleration : 4,
-						scrollSpeed : 800,
-						noScrollCenterSpace : 10,
-						autoScrolling : 0,
-						autoScrollingSpeed : 2000,
-						autoScrollingEasing : "easeInOutQuad",
-						autoScrollingDelay : 500
-					});
-				}
-			})(jQuery);
-		</script>
-		<script src="jquery.thumbnailScroller.js"></script>
-	</body>
+</body>
 </html>
---
+++ resume.php
@@ -0,0 +1,290 @@
+<style>
+	#resumecenter {
+		margin: 0 auto;
+		text-align: center;
+		display: table;
+		width:960px;
+		/* Fallback for web browsers that doesn't support RGBa */
+		background-color: rgb(0, 0, 0); 
+		/* For IE 5.5 - 7*/
+		*zoom: 1;
+		filter:progid:DXImageTransform.Microsoft.gradient(gradientType=0,startColorstr=#99000000,endColorstr=#99000000);
+		-ms-filter:"progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000,endColorstr=#99000000)";
+		background: transparent;
+		/* RGBa with 0.6 opacity */ 
+		background-color: rgba(0,0,0,0.7);
+		color: #FAEBD7;
+		padding-top:10px;
+		padding-left:5px;
+		padding-bottom: 50px;
+
+	}
+	a:link {
+		text-decoration: none;
+		color: #FAEBD7;
+	}
+	a:visited {
+		text-decoration: none;
+		color: #FAEBD7;
+	}
+	a:hover {
+		text-decoration: none;
+		color: #b1b1b1;
+	}
+	a:active {
+		text-decoration: none;
+		color: #FAEBD7;
+	}
+
+
+</style>
+<br /><br />
+<div id="resumecenter">
+<table><tr><td width="520px" align="left" style="font-size: 14px;font-family: verdana; align: left">
+				<b style="color: #FAEBD7;">Name & email:</b>
+				<ul style="list-style-type: none;color: #FAEBD7;">
+					<li style="padding-left:6px">
+						Oana Cosmina Harcau
+					</li>
+					<li style="padding-left:6px">
+						oana_harcau@yahoo.com
+					</li>
+				</ul>
+
+				<br />
+				<b style="color: #FAEBD7;">About:</b>
+				<ul style="list-style-type: none;color: #FAEBD7;">
+					<li style="padding-left:6px">
+						Hello, my name is Oana Cosmina Harcau and I gratueted Polytechnic University of Bucharest
+					</li>
+				</ul>
+				<b style="color: #FAEBD7;">Foreign languages:</b>
+				<ul style="list-style-type: none;color: #FAEBD7;">
+					<li style="padding-left: 6px">
+						English (Advanced) <br />
+						Romanian (Native)
+					</li>
+				</ul>
+				<b style="color: #FAEBD7;">Summary about me:</b>
+				<ul style="list-style-type: none;color: #FAEBD7;">
+					<li style="padding-left: 6px">
+						I'm eager to learn new things and happy to face with all challenges that can appear. <br/>
+						Happy to meet new people and impatient for integrate in different teams.
+						Focused to accomplish all tasks at high quality level and very carefully at details.
+					</li>
+				</ul>
+				<b style="color: #FAEBD7;">Thinking about the job:</b>
+				<ul style="list-style-type: none;color: #FAEBD7;">
+					<li style="padding-left: 6px">
+						Let's start working together! <br />
+						Let's obtain from this job reciprocal benefits for me and for company!
+					</li>
+				</ul>
+				<b style="color: #FAEBD7;">Professional experience:</b>
+				<ul style="color: #FAEBD7;">
+					<li style="padding-left: 6px">
+						<i>ISPE</i>, Bucharest-2006-09 - 2007-11
+
+						<ol>
+							<li style="padding-left: 6px">
+								Piping 2D drawing projects
+							</li>
+
+							<li style="padding-left: 6px">
+								Ploting autocad projects.
+						</ol>
+					</li>
+
+					<li style="padding-left: 6px">
+						<i> VEGRA SRL</i> -2007-11 - present
+
+						<br />
+
+						<ol>
+							<li style="padding-left: 6px">
+								Achievement 2D and 3D building plans
+							</li>
+							<!--
+							<li style="padding-left: 20px">
+							Transform paper sketches or scanned paper to Autocad Drawings ( rastering )
+							</li>
+							<li style="padding-left: 20px">
+							Layer structure - using a structure in accordance with your specifications
+							</li>
+							<li style="padding-left: 20px">
+							Object representation - walls, doors, windows and other objects
+							</li>
+							<li style="padding-left: 20px">
+							Geometric correspondence between floor plans, sections and facades with different companies in the Netherlands and France
+							</li>
+							-->
+							<li style="padding-left: 6px">
+								Achievement  template site from graphical point of view for Vegra
+							</li>
+							<li style="padding-left: 6px">
+								Interior design for many international companies in 3D studio max
+							</li>
+							<li style="padding-left: 6px">
+								Project Management on different projects ( where was necessary to manage different teams )
+							</li>
+							<li style="padding-left: 6px">
+								Created/Modified flayers, photo, banners for Archibus
+							</li>
+							<li style="padding-left: 6px">
+								Graphic magazine pages, cover book graphics for Archibus
+							</li>
+							<li style="padding-left: 6px">
+								Book achievement pages, layout pages, pictures modification in the book pages, book design, cover book design for an Archibus book for an official Archibus meeting that was in Romanian
+							</li>
+
+						</ol>
+					</li>
+					<li style="padding-left: 6px">
+						<i> Freelancer</i> -2007 - present - I worked at all these projects at  home based system for some months/years with some companies or project based system where I received a project and finished it for other companies
+
+						<br />
+
+						<ol>
+							<li style="padding-left: 6px">
+								2D and 3D Buildings for France and Nederland (RPS Kraan) companies
+							</li>
+							<!--
+							<li style="padding-left: 20px">
+							Working on technical part project to obtain European Fonds for Archibus
+							</li>
+							-->
+
+							<li style="padding-left: 6px">
+								Promotion of the &#39;Archibus&#39; product by email campaign
+							</li>
+							<li style="padding-left: 6px">
+								Site for Solidworks Romanian office
+							</li>
+							<li style="padding-left: 6px">
+								Site layout design for : furniture company, dentist company, etc.
+							</li>
+
+				</ul>
+
+				<b style="color: #FAEBD7;">Specialties:</b>
+				<ul style="color: #FAEBD7;">
+					<li style="padding-left: 6px">
+						autocad
+					</li>
+					<li style="padding-left: 6px">
+						photoshop
+					</li>
+					<li style="padding-left: 6px">
+						3d studio max
+					</li>
+				</ul>
+				<b style="color: #FAEBD7;">Skills:</b>
+				<ul style="color: #FAEBD7;">
+					<li style="padding-left: 6px">
+						Advanced level:
+						<ol>
+							<li style="padding-left: 6px">
+								Adobe Photoshop
+							</li>
+							<li style="padding-left: 6px">
+								Autocad
+							</li>
+							<li style="padding-left: 6px">
+								3D Studio Max
+							</li>
+						</ol>
+					</li>
+					<li style="padding-left: 6px">
+						Medium Level:
+						<ol>
+							<li style="padding-left: 6px">
+								Revit Architecture 2010
+							</li>
+							<li style="padding-left: 6px">
+								Illustrator
+							</li>
+							
+						</ol>
+					</li>
+					<li style="padding-left: 6px">
+						Satisfied Level:
+						<ol>
+							<li style="padding-left: 6px">
+								Html, Css
+							</li>
+							<li style="padding-left: 6px">
+								Microsoft Word, Microsoft Excel,Microsoft Outlook
+							</li>
+						</ol>
+					</li>
+				</ul>
+
+</td>
+<td align="left" style="font-size: 14px;font-family: verdana; align: left">
+				<b style="color: #FAEBD7;">Courses, certificates and awards</b>
+				<ul style="color: #FAEBD7;">
+					<b>
+					<li style="padding-left: 3px">
+						AUTOCAD
+					</b>
+					<ol>
+						<li style="padding-left: 6px">
+							<a href="certification/autocad-2D-diploma.jpg" target="_blank"> Autocad level I-2D</a>
+						</li>
+						<li style="padding-left: 6px">
+							<a href="certification/autocad-lev_3D-diploma.jpg" target="_blank"> Autocad level II-3D</a>
+						</li>
+						<li style="padding-left: 6px">
+							<a href="certification/vtcCertificate-34045.pdf" target="_blank"> Autodesk AutoCAD 2010: Certified Professional</a>
+						</li>
+
+					</ol>
+					</li>
+					<b>
+					<li style="padding-left: 3px">
+						3D STUDIO MAX
+					</b>
+					<ol>
+						<li style="padding-left: 6px">
+							<a href="certification/3d-studio-max-level-I-intensive.jpg" target="_blank"> Intensive course 3D Studio Max + Vray</a>
+						</li>
+						<li style="padding-left: 6px">
+							<a href="certification/vtcCertificate-34332.pdf" target="_blank"> Autodesk 3ds Max 2012</a>
+						</li>
+						<li style="padding-left: 6px">
+							<a href="certification/vtcCertificate-34310.pdf" target="_blank"> Concept to 3D Art for the Game Designer</a>
+						</li>
+
+					</ol>
+					</li>
+					
+					<b>
+					<li style="padding-left: 3px">
+						Adobe Photoshop
+					</b>
+					<ol>
+						<li style="padding-left: 6px">
+							<a href="certification/vtcCertificate-34150.pdf" target="_blank"> Adobe Photoshop CS5</a>
+						</li></a>
+						</li>
+					</ol>
+
+					<b>
+					<li style="padding-left: 3px">
+						ZBrush
+					</b>
+					<ol>
+						<li style="padding-left: 6px">
+							<a href="certification/vtcCertificate-34244.pdf" target="_blank"> Pixologic ZBrush 4</a>
+						</li></a>
+						</li>
+
+					</ol>
+					</li>
+
+				</ul>
+
+</td>
+
+</tr></table>
+</div>
